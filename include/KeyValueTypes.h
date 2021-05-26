bde649a9da02f91ade2eeac5b3c57523fb14ca8d
GlebSolovev <lionyoung@mail.ru>
2021-05-26 20:46:43 +0300

    Format code, add final and fix some TODO-s

--- include/KeyValueTypes.h
+++ include/KeyValueTypes.h
@@ -7,28 +7,26 @@ constexpr size_t KEY_SIZE = 16;
 constexpr size_t CACHE_MAP_SIZE = 5000;
 constexpr double MAP_LOAD_FACTOR = 1.5;
 constexpr size_t SHARD_NUMBER = 4981;
-constexpr size_t BLOOM_FILTER_SIZE = 5;                  // TODO
+constexpr size_t BLOOM_FILTER_SIZE = 5; // TODO
 constexpr size_t BLOOM_FILTER_HASH_FUNCTIONS_NUMBER = 5; // TODO
 
 using key_t = __uint128_t;
 using value_t = char*;
 using ptr_t = char;
 
-class Key {
-
-  public:
-    key_t get();
+final class Key {
+public:
+  key_t get();
 };
 
-class Value {
-
-  public:
-    value_t get();
+final class Value {
+public:
+  value_t get();
 };
 
-struct KeyValue {
-    Key key;
-    Value value;
+final struct KeyValue {
+  Key key;
+  Value value;
 };
 
 /**
@@ -36,21 +34,21 @@ struct KeyValue {
  * information whether the associated value is present or deleted.
  *
  */
-class Ptr {
+final class Ptr {
 
-  public:
-    ptr_t get() const;
+public:
+  ptr_t get() const;
 
-    /**
+  /**
      * @brief Check if this Ptr points to a present or deleted value.
      *
      */
-    bool isValuePresent() const;
+  bool isValuePresent() const;
 
-    /**
+  /**
      * @brief Set state for the value associated with this Ptr.
      *
      * @param isPresent
      */
-    void setValuePresent(bool isPresent);
+  void setValuePresent(bool isPresent);
 };
