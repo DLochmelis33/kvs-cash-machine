84142ce14026cc1e344c643f1115f185cf499b17
DLochmelis33 <DLochmelis33@users.noreply.github.com>
2021-06-28 12:47:09 +0000

    deploy: f2130c467754c5235d92bf6ca3559a156dd598b2

--- test/TestKVS.cpp
+++ test/TestKVS.cpp
@@ -1,36 +1,102 @@
 #include "KVS.h"
 #include "doctest.h"
 #include <cstring>
+#include <random>
+#include <sstream>
+#include <unordered_set>
+
+namespace std {
+
+// required for std::unordered_set<Key>
+template <>
+struct hash<kvs::utils::Key> {
+  std::size_t operator()(const kvs::utils::Key& key) const noexcept {
+    return kvs::utils::hashKey(key, 0);
+  }
+};
+
+} // namespace std
 
 using namespace kvs;
 using namespace kvs::utils;
 
 namespace test_kvs::kvs {
 
-Key generateKey(size_t value) {
-  ByteArray byteArray(KEY_SIZE);
-  std::memcpy(byteArray.get(), reinterpret_cast<char*>(&value), sizeof(size_t));
-  return Key{byteArray};
+std::random_device rd;
+std::mt19937_64 gen(rd());
+std::uniform_int_distribution<char> charDistr(std::numeric_limits<char>::min(),
+                                              std::numeric_limits<char>::max());
+
+ByteArray generateRandomByteArray(size_t size) {
+  ByteArray bytes(size);
+  char* charptr = bytes.get();
+  for (size_t i = 0; i < size; ++i) {
+    charptr[i] = charDistr(gen);
+  }
+  return bytes;
 }
 
-Value generateValue(int seed) {
-    srand(seed);
-    
+Key generateRandomKey() { return Key{generateRandomByteArray(KEY_SIZE)}; }
+
+Value generateRandomValue() {
+  return Value{generateRandomByteArray(VALUE_SIZE)};
 }
 
-const Ptr p1(1 * VALUE_SIZE, true), p2(2 * VALUE_SIZE, true),
-    p3(3 * VALUE_SIZE, true);
-const Entry e1(generateKey(1), p1), e2(generateKey(2), p2),
-    e3(generateKey(3), p3);
-const Entry e4(generateKey(4), p1), e5(generateKey(5), p2),
-    e6(generateKey(6), p3);
+Key generateNewRandomKey(std::unordered_set<Key>& keys) {
+  Key key = generateRandomKey();
+  while (keys.find(key) != keys.end()) {
+    Key key = generateRandomKey();
+  }
+  keys.insert(key);
+  return key;
+}
 
+std::string byteArrayToStr(ByteArray ba) {
+  std::stringstream ss;
+  for (size_t i = 0; i < ba.length(); i++) {
+    ss << (int) ba.get()[i] << ' ';
+  }
+  return ss.str();
+}
 
 TEST_CASE("test KVS") {
 
   SUBCASE("test simple") {
     KVS kvs;
-    kvs.add()
+
+    Key k1 = generateRandomKey();
+    Value v1 = generateRandomValue();
+    kvs.add(k1, v1);
+    std::optional<Value> optVal = kvs.get(k1);
+    CHECK(optVal.has_value());
+    CHECK(optVal.value() == v1);
+
+    Key k2 = generateRandomKey();
+    Value v2 = generateRandomValue();
+    kvs.add(k2, v2);
+    optVal = kvs.get(k2);
+    CHECK(optVal.has_value());
+    CHECK(optVal.value() == v2);
+  }
+
+  SUBCASE("test simple remove") {
+    KVS kvs;
+
+    Key k1 = generateRandomKey();
+    Value v1 = generateRandomValue();
+    kvs.add(k1, v1);
+
+    Key k2 = generateRandomKey();
+    Value v2 = generateRandomValue();
+    kvs.add(k2, v2);
+
+    std::optional<Value> optVal = kvs.get(k1);
+    CHECK(optVal.has_value());
+    CHECK(optVal.value() == v1);
+
+    kvs.remove(k1);
+    optVal = kvs.get(k1);
+    CHECK(!optVal.has_value());
   }
 }
 
